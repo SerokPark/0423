68a9c11f1d205f9f7fe3b43a73473c18bf4c32ad
SerokPark <dkdlaqhsl@pusan.ac.kr>
2024-04-23 17:19:23 +0900

    Create Multi5.cpp

---
+++ Multi5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+	int i = 5;
+	std::cout << i << "´Ü\n";
+	int j;
+	for (int j = 1; j <= 9; j++)
+	{
+		std::cout << i << "*" << j << "=" << i * j << "\n";
+		
+		
+	}
+}
